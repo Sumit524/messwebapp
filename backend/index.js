09bf0d8b438f7b33f806e8ef121fc2e286f80d65
Sumit524 <125038496+Sumit524@users.noreply.github.com>
2024-07-19 21:50:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const PORT =  process.env.PORT||3000;
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://messwebapp-tzzh.vercel.app",
   credentials: true,
 }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
